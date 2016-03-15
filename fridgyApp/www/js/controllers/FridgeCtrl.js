eb3ae428c461dab4b0a2702761a7edad16684163
Diego Agudelo <diego.agudelo2@gmail.com>
2016-03-14 23:41:51 -0500

    Lista mi nevera y neveras cercanas con datos reales

--- fridgyApp/www/js/controllers/FridgeCtrl.js
+++ fridgyApp/www/js/controllers/FridgeCtrl.js
@@ -1,14 +1,18 @@
 
-app.controller('FridgeCtrl', function ($scope, $stateParams, ionicMaterialMotion, $http) {
+app.controller('FridgeCtrl', function ($scope, $stateParams, ionicMaterialMotion, $http, $firebaseObject) {
 
   $scope.blinds = function() {
-    $http.get("/resources/fridges/" + $stateParams.id + ".json").then(function(data){
-      $scope.fridge = data.data;
-      document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
-      setTimeout(function() {
-        ionicMaterialMotion.blinds();
-      }, 0);
-    });
+
+    var fridgeRef = new Firebase("https://carmudgeon.firebaseio.com/fridges/"+$stateParams.id);
+    $scope.fridge =  $firebaseObject(fridgeRef);
+
+    //$http.get("/resources/fridges/" + $stateParams.id + ".json").then(function(data){
+    //  $scope.fridge = data.data;
+    //  document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
+    //  setTimeout(function() {
+    //    ionicMaterialMotion.blinds();
+    //  }, 0);
+    //});
   };
 
   $scope.blinds();
